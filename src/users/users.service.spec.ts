8cb824801d2d6232dbab0edbba58404cc8f89a4b
BuildTools <unconfigured@null.spigotmc.org>
2023-02-27 21:42:00 -0300

    primeiro commit

---
+++ src/users/users.service.spec.ts
@@ -0,0 +1,151 @@
+import { describe } from 'node:test';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { UsersService } from './users.service';
+
+describe('UsersService', () => {
+  let usersService: UsersService;
+
+  beforeEach(() => {
+    usersService = new UsersService();
+  });
+
+  describe('create', () => {
+    it('should return an user after its creation', async () => {
+      // Setup - cria o ambiente do teste
+      const userDto = new CreateUserDto();
+      userDto.idade = 12;
+      userDto.nome = "Binha";
+
+      // Execution - executa o teste
+      // userCreated é o resultado do teste
+      const userCreated = usersService.create(userDto);
+
+      // Assertion - valida o teste e mostra o resultado
+      expect(userCreated.id).toBe(1);
+      expect(userCreated.idade).toBe(12);
+      expect(userCreated.nome).toBe("Binha");
+    });
+
+    it('should return each user after two creations', async () => {
+      // Setup - cria o ambiente do teste
+      const userDto1 = new CreateUserDto();
+      userDto1.idade = 28;
+      userDto1.nome = "Binha";
+
+      const userDto2 = new CreateUserDto();
+      userDto2.idade = 26;
+      userDto2.nome = "Biela";
+
+      // Execution - executa o teste
+      // userCreated é o resultado do teste
+      const userCreated1 = usersService.create(userDto1);
+      const userCreated2 = usersService.create(userDto2);
+
+      // Assertion - valida o teste e mostra o resultado
+      expect(userCreated1.id).toBe(1);
+      expect(userCreated1.idade).toBe(28);
+      expect(userCreated1.nome).toBe("Binha");
+
+      expect(userCreated2.id).toBe(2);
+      expect(userCreated2.idade).toBe(26);
+      expect(userCreated2.nome).toBe("Biela");
+    });
+  });
+/*
+  describe('findAll', () => {
+    it('should return all users created', async () => {
+      // Setup - cria o ambiente do teste
+
+      const userDto1 = new CreateUserDto();
+      userDto1.idade = 50;
+      userDto1.nome = "Vander";
+
+      const userDto2 = new CreateUserDto();
+      userDto2.idade = 70;
+      userDto2.nome = "Nildo";
+
+      // Execution - executa o teste
+      // userCreated é o resultado do teste
+      usersService.create(userDto1)
+      usersService.create(userDto2)
+      const returnedUsers = usersService.findAll();
+
+      // cada objeto fica em um slot separado da memoria
+      // Assertion - valida o teste e mostra o resultado
+      expect(returnedUsers[0]).toEqual({
+        id: 1,
+        ...userDto1,
+      });
+      expect(returnedUsers[1]).toEqual({
+        id: 2,
+        ...userDto2,
+      });
+      expect(returnedUsers.length).toBe(2);
+    });
+  });
+  describe('findOne', () => {
+    it('should find a existing user', async () => {
+      // Setup - cria o ambiente do teste
+      const userDto = new CreateUserDto();
+      userDto.idade = 18;
+      userDto.nome = "Luffy";
+
+      // Execution - executa o teste
+      // userCreated é o resultado do teste
+      usersService.create(userDto)
+
+      const returnedOneUser = usersService.findOne(1);
+
+      // cada objeto fica em um slot separado da memoria
+      // Assertion - valida o teste e mostra o resultado
+      
+      expect(returnedOneUser.id).toBe(1);
+    });
+  });
+  describe('update', () => {
+    it('should update a user', async () => {
+      // Setup - cria o ambiente do teste
+      const userDto = new CreateUserDto();
+      userDto.idade = 50;
+      userDto.nome = "Vander";
+      const updateUserDto = new UpdateUserDto;
+
+      // Execution - executa o teste
+      // userCreated é o resultado do teste
+      usersService.create(userDto);
+      usersService.findOne(1);
+      const updatedName = usersService.update(1, updateUserDto);{
+        nome : 'Vanderson' ;
+      };
+      const updatedUser = {
+        ...userDto,
+        ...updatedName,
+      }
+      // cada objeto fica em um slot separado da memoria
+      // Assertion - valida o teste e mostra o resultado
+
+      expect(updatedUser.nome).toBe('Vanderson')
+    })
+  })
+  describe('delete', () => {
+    it('should delete a user', async () => {
+      // Setup - cria o ambiente do teste
+      const userDto = new CreateUserDto();
+      userDto.idade = 20;
+      userDto.nome = "Moria";
+
+      // Execution - executa o teste
+      // userCreated é o resultado do teste
+      usersService.create(userDto);
+      usersService.findOne(1);
+      const deleteUser = usersService.remove(1);
+      
+      // cada objeto fica em um slot separado da memoria
+      // Assertion - valida o teste e mostra o resultado
+
+      expect(deleteUser).toBe(true);
+    })
+  }) 
+*/
+});
---
+++ src/users/users.service.ts
@@ -0,0 +1,60 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+
+@Injectable()
+export class UsersService {
+  private users: User[] = [];
+
+  getLastUserId(): number | undefined {
+    const lastUser = this.users[this.users.length - 1]
+    return lastUser?.id
+  }
+
+  create(createUserDto: CreateUserDto) {
+    const currentMaxId = this.getLastUserId() || 0;
+
+    const user = {
+      id: currentMaxId + 1,
+      ...createUserDto,
+    };
+
+    this.users.push(user);
+
+    return user;
+  }
+
+  findAll() {
+    return this.users;
+  }
+
+  findOne(id: number) {
+
+    const index = this.users.findIndex((user)=> user.id === id);
+    return this.users[index];
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+
+    const user = this.findOne(id);
+
+    const newUser = {
+      ...user,
+      ...updateUserDto,
+    };
+
+    const index = this.users.findIndex((user)=> user.id === id);
+
+    this.users[index] = newUser;
+    
+    return newUser;
+  }
+
+  remove(id: number) {
+
+    const index = this.users.findIndex((user)=> user.id === id);
+
+    this.users.splice(index, 1);
+  }
+}
