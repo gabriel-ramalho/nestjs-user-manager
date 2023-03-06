2b49cef10ef6377464e9782a807bb3253dd7aeeb
BuildTools <unconfigured@null.spigotmc.org>
2023-03-06 19:48:18 -0300

    mais testes

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -86,48 +86,79 @@ describe('UsersService', () => {
   describe('findOne', () => {
     it('should find a existing user', async () => {
       // Setup - cria o ambiente do teste
-      const userDto = new CreateUserDto();
-      userDto.idade = 18;
-      userDto.nome = "Luffy";
+      const userDto1 = new CreateUserDto();
+      userDto1.idade = 18;
+      userDto1.nome = "Luffy";
+
+      const userDto2 = new CreateUserDto();
+      userDto2.idade = 54;
+      userDto2.nome = "Garp";
 
       // Execution - executa o teste
       // userCreated é o resultado do teste
-      usersService.create(userDto)
+      usersService.create(userDto1)
+      usersService.create(userDto2)
 
-      const returnedOneUser = usersService.findOne(1);
+      const returnedOneUser1 = usersService.findOne(1);
+      const returnedOneUser2 = usersService.findOne(2);
+      const returnedUserUnderfined = usersService.findOne(3);
 
       // cada objeto fica em um slot separado da memoria
       // Assertion - valida o teste e mostra o resultado
       
-      expect(returnedOneUser).toEqual({
+      expect(returnedOneUser1).toEqual({
         id : 1,
         nome: "Luffy",
         idade: 18,
       })
+      expect(returnedOneUser2).toEqual({
+        id : 2,
+        nome: "Garp",
+        idade: 54,
+      })
+      expect(returnedUserUnderfined).toBeUndefined();
     });
 
   });
   describe('update', () => {
     it('should update a user', async () => {
       // Setup - cria o ambiente do teste
-      const userDto = new CreateUserDto();
-      userDto.idade = 50;
-      userDto.nome = "Vander";
-      const updateUserDto = new UpdateUserDto();
-      updateUserDto.nome = "Vanderson";
+      const userDto1 = new CreateUserDto();
+      userDto1.idade = 50;
+      userDto1.nome = "Vander";
+
+      const userDto2 = new CreateUserDto();
+      userDto2.idade = 34;
+      userDto2.nome = "Tiago";
+
+      const updateUserDto1 = new UpdateUserDto();
+      updateUserDto1.nome = "Vanderson";
+
+      const updateUserDto2 = new UpdateUserDto();
+      updateUserDto2.idade = 35;
+
       // Execution - executa o teste
       // userCreated é o resultado do teste
-      usersService.create(userDto);
+      usersService.create(userDto1);
       usersService.findOne(1);
-      const updatedUser = usersService.update(1, updateUserDto);
+      const updatedUser1 = usersService.update(1, updateUserDto1);
+
+      usersService.create(userDto2);
+      usersService.findOne(2);
+      const updatedUser2 = usersService.update(2, updateUserDto2);
 
       // cada objeto fica em um slot separado da memoria
       // Assertion - valida o teste e mostra o resultado
 
-      expect(updatedUser).toEqual({
+      expect(updatedUser1).toEqual({
         id: 1,
         nome: "Vanderson",
         idade: 50
+      });
+      expect(updatedUser2).toEqual({
+        id: 2,
+        nome: "Tiago",
+        idade: 35
       });
     });
   });
@@ -144,14 +175,17 @@ describe('UsersService', () => {
       const beforDeleteUser = usersService.findOne(1);
       
       usersService.remove(1);
-      const deleteUser = usersService.findOne(1);
+      usersService.remove(2);
+      const deleteUser1 = usersService.findOne(1);
+      const deleteUser2 = usersService.findOne(2);
 
       
       // cada objeto fica em um slot separado da memoria
       // Assertion - valida o teste e mostra o resultado
 
       expect(beforDeleteUser).not.toBeUndefined();
-      expect(deleteUser).toBeUndefined();
+      expect(deleteUser1).toBeUndefined();
+      expect(deleteUser2).toBeUndefined();
     })
   }) 
 
